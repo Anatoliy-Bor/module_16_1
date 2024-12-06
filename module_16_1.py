ac229a18071c1ff6edf372e5a5c9cdddb27d97e3
Anatoliy-Bor <89204220661@mail.ru>
2024-12-06 16:22:01 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root() -> dict:
+    return {"msg":"Главная страница !"}
+
+@app.get("/user/admin")
+async def read_admin() -> dict:
+    return {"msg":"Вы зашли как администратор !"}
+
+@app.get("/user/{user_id}")
+async def get_user(user_id: int) -> dict:
+    return {"msg":f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/id")
+async def tak_user(username: str, age: int) -> dict:
+    return {"User": username, "Age": age}
+
+@app.post("/products/")
+async def create_product():
+    """
+Создает новый продукт в системе.
+- **name**: название продукта
+- **price**: цена продукта
+- **quantity**: количество на складе
+"""
+    return
